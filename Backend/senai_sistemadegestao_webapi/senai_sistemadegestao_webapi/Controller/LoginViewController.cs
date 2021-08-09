ac6fce1bf19bc6140de449cdf944689e306d97e7
Regis Magalhães <regis.silva3@senaisp.edu.br>
2021-08-09 16:26:21 -0300

    Login finalizado, testar

--- Backend/senai_sistemadegestao_webapi/senai_sistemadegestao_webapi/Controller/LoginViewController.cs
+++ Backend/senai_sistemadegestao_webapi/senai_sistemadegestao_webapi/Controller/LoginViewController.cs
@@ -1,15 +1,112 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using senai_sistemadegestao_webapi.Domains;
+using senai_sistemadegestao_webapi.Interfaces;
+using senai_sistemadegestao_webapi.Repositories;
+using senai_sistemadegestao_webapi.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace senai_sistemadegestao_webapi.Controller
 {
+    /// <summary>
+    /// Controller responsável pelos endpoints do Login
+    /// </summary>
+    /// 
+
+    //Define que o formato de resposta da api será no tipo json
+    [Produces("aplication/json")]
+    //Define que a rota da requisição será no formato dominio/api/nomeController
     [Route("api/[controller]")]
+    //Define que é um controlador de api
     [ApiController]
     public class LoginViewController : ControllerBase
     {
+
+        /// <summary>
+        /// Objeto que irá receber os métodos definidos na interface IUsuarioRepository
+        /// </summary>
+        private IUsuarioRepository _usuarioRepository { get; set; }
+
+        //Instancia o objeto _usuarioRepository para que haja as referência aos métodos do repositorios
+        public LoginViewController()
+        {
+            _usuarioRepository = new UsuarioRepository();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel login)
+        {
+            try
+            {
+                //Busca o usuario pelo email e senha
+                Usuario usuarioBuscado = _usuarioRepository.Login(login.Email, login.Senha);
+
+                if (usuarioBuscado == null)
+                {
+                    //Caso não encontre usuario buscado
+                    return NotFound("Email ou senha inválidos!");
+                }
+
+                // Caso o usuário seja encontrado, prossegue para a criação do token
+
+                /*
+                    Dependências
+                    Criar e validar o JWT:      System.IdentityModel.Tokens.Jwt
+                    Integrar a autenticação:    Microsoft.AspNetCore.Authentication.JwtBearer (versão compatível com o .NET do projeto)
+                */
+
+                // Define os dados que serão fornecidos no token - Payload
+                var claims = new[]
+                {
+                    // Armazena na Claim o e-mail do usuário autenticado
+                    new Claim(JwtRegisteredClaimNames.Email, usuarioBuscado.Email),
+
+                    // Armazena na Claim o ID do usuário autenticado
+                    new Claim(JwtRegisteredClaimNames.Jti, usuarioBuscado.IdUsuario.ToString()),
+
+                    // Armazena na Claim o tipo de usuário que foi autenticado (Administrador ou Comum)
+                    new Claim(ClaimTypes.Role, usuarioBuscado.IdUsuario.ToString()),
+
+                    // Armazena na Claim o tipo de usuário que foi autenticado (Administrador ou Comum) de forma personalizada
+                    new Claim("role", usuarioBuscado.IdUsuario.ToString()),
+
+
+                };
+
+                // Define a chave de acesso ao token
+                var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("patrimonio-chave-autenticacao"));
+
+                // Define as credenciais do token - Header
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                // Gera o token
+                var token = new JwtSecurityToken(
+                    issuer: "sistemadegestao.webApi",                 // emissor do token
+                    audience: "sistemadegestao.webApi",               // destinatário do token
+                    claims: claims,                        // dados definidos acima
+                    expires: DateTime.Now.AddMinutes(30),  // tempo de expiração
+                    signingCredentials: creds              // credenciais do token
+                );
+
+                // Retorna Ok com o token
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+
+
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex);
+            }
+        }
     }
 }
